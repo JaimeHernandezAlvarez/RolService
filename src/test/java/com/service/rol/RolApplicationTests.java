31693d0717deeadeeba559f603815dcfcc7987ea
Jaime <jai.hernandeza@duocuc.cl>
2025-06-05 13:25:07 -0400

    Servicio Rol Inicio

---
+++ src/test/java/com/service/rol/RolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.rol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
